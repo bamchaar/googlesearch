7332d3f23f4d52e2c2cb015e12512cdc871b1f82
Amine <amineatya@gmail.com>
2022-09-08 02:53:55 -0400

    initial commit

---
+++ build/precache-manifest.c9d62f21381b2f3e4908a3f3e01ba327.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8452915085eb23660735e287db39acbb",
+    "url": "/amineatya/Google_books/index.html"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/amineatya/Google_books/static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "3e37668c3c1516f1abb7",
+    "url": "/amineatya/Google_books/static/css/main.99d2c493.chunk.css"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/amineatya/Google_books/static/js/2.857712a8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/amineatya/Google_books/static/js/2.857712a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e37668c3c1516f1abb7",
+    "url": "/amineatya/Google_books/static/js/main.4dca19a6.chunk.js"
+  },
+  {
+    "revision": "7e7c11268742b3f60af3",
+    "url": "/amineatya/Google_books/static/js/runtime-main.76fe44b7.js"
+  }
+]);
